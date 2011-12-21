c25c50b436118b13f5496e764319cee13f80f893
X Kerman <xKhorasan@gmail.com>
2011-12-21 23:52:59 +0900

    add README and install script.

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+
+setup(name='glitch',
+      version='0.0.1',
+      description='glitch utility module',
+      author='xKerman',
+      author_email='xKhorasan@gmail.com',
+      url='https://github.com/xKerman',
+      package_dir={'glitch': 'src'},
+      packages=['glitch'])
