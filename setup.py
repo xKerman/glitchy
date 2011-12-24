7b497e618b5c52febe286bebe1dae2d259b75fdd
X Kerman <xKhorasan@gmail.com>
2011-12-25 08:04:58 +0900

    fix setup.py for module name.

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 
-setup(name='glitch',
+setup(name='glitchy',
       version='0.0.1',
       description='glitch utility module',
       author='xKerman',
-      author_email='xKhorasan@gmail.com',
-      url='https://github.com/xKerman',
+      author_email='eEtob3Jhc2FuQGdtYWlsLmNvbQ==\n'.decode('base64'),
+      url='https://github.com/xKerman/glitchy',
       package_dir={'glitchy': 'src'},
       packages=['glitchy'])
